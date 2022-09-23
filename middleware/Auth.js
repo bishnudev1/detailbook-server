f5e38bc199db3b99448c7ff1be5bff1679ac3041
Bishnudev Khutia <91979889+bishnudev1@users.noreply.github.com>
2022-09-23 23:05:54 +0530

    Update Auth.js

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -4,11 +4,20 @@ const jwt = require('jsonwebtoken');
 module.exports = async function Auth (req,res,next){
     try {
         const token = req.cookies.jwt;
-        const verifyToken = jwt.verify(token,'Iambishnudevkhutia');
-        const user = await User.findOne({_id:verifyToken._id});
-        req.token = token;
-        req.user = user;
-        next();
+        
+           if(!token){
+                res.status(404).json({
+                    message:'User not logged in');
+                });
+               next();
+           }
+            else{
+                const verifyToken = jwt.verify(token,'Iambishnudevkhutia');
+                const user = await User.findOne({_id:verifyToken._id});
+                req.token = token;
+                req.user = user;
+                next();
+            }
     } catch (error) {
         console.log(error);
     }
