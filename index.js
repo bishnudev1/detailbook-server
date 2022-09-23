a15f0578049dfbd4f3ae5746ce12dcf3d6b87625
bishnudev1 <bishnudevkhutia@gmail.com>
2022-09-23 23:39:46 +0530

    db-serve

--- index.js
+++ index.js
@@ -12,13 +12,9 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-const corsOptions ={
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
 
+app.use(cors());
 app.use(express.json());
-app.use(cors(corsOptions));
 app.use(cookieParser());
 
 require('./db/conn');
