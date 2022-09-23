7353a290644cff220870fb8e272a8caf9f315c3d
bishnudev1 <bishnudevkhutia@gmail.com>
2022-09-23 23:33:30 +0530

    update

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 const corsOptions ={
-    origin:'https://detailbook-server.herokuapp.com', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
  }
